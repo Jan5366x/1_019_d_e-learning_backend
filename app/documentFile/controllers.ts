9e208b7885e52d560b760b82d8afa8608a29ab03
Denise Wagenführ <dwagenfuehr@jlu.de>
2020-03-21 20:30:48 +0100

    createBucket Method

--- app/documentFile/controllers.ts
+++ app/documentFile/controllers.ts
@@ -1,7 +1,26 @@
 import { RequestHandler, Request, Response } from "express";
+import { S3, Endpoint } from "aws-sdk";
+import ExpressError from "../classes/ExpressError";
 
 const PostFile: RequestHandler = (req: Request, res: Response) => {
     res.status(200).json({ message: "OK!" });
 };
 
-export { PostFile };
+const CreateBucket: RequestHandler = (req: Request, res: Response, next: Function) => {
+    const s3 = new S3({
+        accessKeyId: '',
+        secretAccessKey: ''
+    });
+    const bucketName = req.body.bucketName.toLowerCase();
+    if (bucketName == null) return next(new ExpressError("BUCKETNAME_REQUIRED", "You have to provide a bucket name.", 400));
+    s3.createBucket({ Bucket: bucketName }, function (err, data) {
+        if (err) {
+            return next(new ExpressError(err.message, "Bucket could not be created.", 400));
+        } else {
+            res.status(201).json({ message: "Successfully created bucket: " + bucketName });
+        }
+    });
+};
+
+
+export { PostFile, CreateBucket };
--- app/documentFile/routes.ts
+++ app/documentFile/routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import {
     PostFile as PostFileController,
+    CreateBucket as CreateBucketController,
 } from "./controllers";
 
 const router: express.Router = express.Router();
 
+router.post("/createBucket", CreateBucketController);
 router.get("/upload", PostFileController);
 
 export default router;
