fefc9e9f61dbfde6ae53cd5d98a468e23b72e774
Justus Fluegel <klettelars@gmail.com>
2020-03-21 17:59:05 +0100

    add Signup

--- app/classes/ExpressError.ts
+++ app/classes/ExpressError.ts
@@ -1,5 +1,11 @@
 class ExpressError extends Error {
     public status?: number;
+    public humanReadableError?: string;
+    constructor(message?: string, humanReadableError?: string, statusCode?: number) {
+        super(message)
+        this.status = statusCode;
+        this.humanReadableError = humanReadableError;
+    }
 }
 
 export default ExpressError;
--- app/user/model.ts
+++ app/user/model.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true },
     username: {
         type: String,
         required: true,
-        minlength: 3,
+        minlength: 6,
         maxlength: 50
     },
     name: {
@@ -20,7 +20,6 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        minlength: 5,
         maxlength: 255,
         unique: true,
         match: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
@@ -33,7 +32,26 @@ const UserSchema = new mongoose.Schema({
     },
     permissions: [
         { type: String }
-    ]
+    ],
+    role: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+    }
 });
 
-export default mongoose.model("User", UserSchema);
+const userModel: mongoose.Model<mongoose.Document> = mongoose.model("User", UserSchema);
+
+const RoleSchema = new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    name: {
+        type: String,
+        minlength: 3,
+        maxlength: 64,
+        required: true,
+        unique: true
+    }
+})
+
+const roleModel: mongoose.Model<mongoose.Document> = mongoose.model("Role", RoleSchema);
+
+export { userModel as UserM, roleModel as RoleM };
