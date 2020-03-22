e8225a85d96c9b38993c4e0d343e4ea7a0ddf886
Denise Wagenführ <dwagenfuehr@jlu.de>
2020-03-22 13:56:47 +0100

    .
    Co-authored-by: Technikkeller <klelars@outlook.de>

--- app/documentFile/controllers.ts
+++ app/documentFile/controllers.ts
@@ -1,10 +1,12 @@
 import { RequestHandler, Request, Response } from "express";
+const formidable = require('formidable')
 import { S3 } from "aws-sdk";
+import { readFile, writeFileSync } from "fs";
 import ExpressError from "../classes/ExpressError";
 
 const s3 = new S3({
-    accessKeyId: $ACCESS_KEY,
-    secretAccessKey: $SECRET_ACCESS_KEY
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
 });
 
 const CreateBucket: RequestHandler = (req: Request, res: Response, next: Function) => {
@@ -12,52 +14,63 @@ const CreateBucket: RequestHandler = (req: Request, res: Response, next: Functio
     const bucketName = req.body.bucketName.toLowerCase();
     if (bucketName == null) return next(new ExpressError("BUCKETNAME_REQUIRED", "You have to provide a bucket name.", 400));
     s3.createBucket({ Bucket: bucketName }, function (err, data) {
-        if (err) {
+        if (err) 
             return next(new ExpressError(err.message, "Bucket could not be created.", 400));
-        } else {
-            res.status(201).json({ message: "Successfully created bucket: " + bucketName });
-        }
+        res.status(201).json({ message: "Successfully created bucket: " + bucketName });
     });
 };
 
 const PostFile: RequestHandler = (req: Request, res: Response, next: Function) => {
-    const bucketName = req.body.bucketName.toLowerCase();
-    if (bucketName == null) return next(new ExpressError("BUCKETNAME_REQUIRED", "You have to provide a bucket name.", 400));
-    const params = {
-        ACL: 'private',
-        Body: buffer,
-        Bucket: `${bucketName}`,
-        Key: `${name}`
-      };
-    s3.upload(params, function (err, data) {
-        if (err) {
-            return next(new ExpressError(err.message, "File could not be uploaded", 400));
-        } else {
-            res.status(201).json({ message: "Successfully uploaded file."});
-        }
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err: any, fields: any, files: any) => {
+        if (err) 
+            return next(new ExpressError("FILEUPLOAD_ERROR", "File could not be uploaded.", 400));
+
+            const { bucketName } = fields;
+            const { file } = files;
+            if (bucketName == null) return next(new ExpressError("BUCKETNAME_REQUIRED", "You have to provide a bucket name.", 400));
+            readFile(file.path, function (err, data) {
+                if (err) {
+                    return next(new ExpressError("FILEUPLOAD_ERROR", "File could not be uploaded.", 400));
+                } else {
+                    const params = {
+                        ACL: 'private',
+                        Body: data,
+                        Bucket: `${bucketName}`,
+                        Key: `${file.name}`
+                    };
+                    s3.upload(params, function (err : any, data : any) {
+                        if (err) {
+                            return next(new ExpressError(err.message, "File could not be uploaded", 400));
+                        } else {
+                            res.status(201).json({ message: "Successfully uploaded file." });
+                        }
+                    });
+                }
+            });
+
+        
     });
+
+
 };
 
 const GetFile: RequestHandler = (req: Request, res: Response, next: Function) => {
-    const bucketName = req.params.bucketName.toLowerCase();
-    const key = req.params.awsKey;
+    const { bucketName, awsKey } = req.query;
     if (bucketName == null) return next(new ExpressError("BUCKETNAME_REQUIRED", "You have to provide a bucket name.", 400));
-    if (key == null) return next(new ExpressError("AWSKEY_REQUIRED", "You have to provide a file specific key.", 400));
-
+    if (awsKey == null) return next(new ExpressError("AWSKEY_REQUIRED", "You have to provide a file specific key.", 400));
     const params = {
         Bucket: `${bucketName}`,
-        Key: `${key}`
-      };
-      const stream = s3.getObject(params).createReadStream();
-      // forward errors
-      stream.on('error', next(new ExpressError("FILE_COULD_NOT_BE_READ", "File could not be read.", 400)));
-
-      stream.on('end', () => {
-        // console.log(`read file ${file}`);
+        Key: `${awsKey}`
+    };
+    console.log(bucketName);
+    s3.getObject(params, (err, data) => {
+        if (err) return next(new ExpressError("FILE_NOT_READABLE", "File could not be read.", 400));
+        console.log(data);
+        writeFileSync(data.filename, data.Body.toString());
+        console.log(`${filePath} has been created!`);
       });
-      // Pipe the s3 object to the response
-      stream.pipe(res);
-      return;
+    return;
 };
 
 
@@ -70,14 +83,14 @@ const DeleteFile: RequestHandler = (req: Request, res: Response, next: Function)
     const params = {
         Bucket: `${bucketName}`,
         Key: `${key}`
-      };
-      const stream = s3.deleteObject(params, function (err, data) {
-          if (err){
+    };
+    const stream = s3.deleteObject(params, function (err, data) {
+        if (err) {
             return next(new ExpressError(err.message, "File could not be deleted", 400));
-          }else{
-            res.status(200).json({ message: "Successfully deleted file."});
-          }
-      });
+        } else {
+            res.status(200).json({ message: "Successfully deleted file." });
+        }
+    });
 };
 
 export { PostFile, CreateBucket, GetFile, DeleteFile };
