bcd04c55113abdb18fbe455f90a5b48a1f613e95
Denise Wagenführ <dwagenfuehr@jlu.de>
2020-03-22 15:46:40 +0100

    s3 config

--- config.ts
+++ config.ts
@@ -6,6 +6,11 @@ export default ({
         password: null,
         database: "test"
     },
-    jsonwebtoken: "xcvhojpüoizuxzcvbonpüpzxytzrczvubioixyterxv33467iouboiüucixuryxcvnmnbhgfgbinpcxyxrvzbunnbvtx6yxcvbnmiuzurzewaerdizuoztshdfgz0978s5dtb"
+    jsonwebtoken: "xcvhojpüoizuxzcvbonpüpzxytzrczvubioixyterxv33467iouboiüucixuryxcvnmnbhgfgbinpcxyxrvzbunnbvtx6yxcvbnmiuzurzewaerdizuoztshdfgz0978s5dtb",
+    s3: {
+        accessKeyId: "AKIASVLWJCWHFTZEMH66",
+        secretAccessKey: "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",
+        uniqueBucketIdentifier: "trxczujokdfxzgh09d7s657rt9z80gtxry5xciuob75eydfizu0gfcxcboiu7id65tzbjih079x68tcz"
+    }
     
 });
