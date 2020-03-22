c4dfc86ba52b1196b0318788878a4b4e173efbb5
Justus Fluegel <klettelars@gmail.com>
2020-03-22 16:38:26 +0100

    add PlannedLessonModel

--- app/lesson/model.ts
+++ app/lesson/model.ts
@@ -2,17 +2,26 @@ import mongoose from "mongoose";
 //import Step from "../step/model"; 
 
 const LessonSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true },
     title: {
         type: String,
         required: true,
         minlength: 3,
         maxlength: 255
-    },
-    date:{
-        type: Date
     }
     //steps: [Step]
 });
 
-export default mongoose.model("Lesson", LessonSchema);
+const LessonM = mongoose.model("Lesson", LessonSchema)
+
+const PlannedLessonSchema = new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    date: {
+        type: Date
+    },
+    course: { type: mongoose.Schema.Types.ObjectId, ref: "Lesson", required: true }
+});
+
+const PlannedLessonM = mongoose.model("PlannedLesson", PlannedLessonSchema);
+
+export { LessonM, PlannedLessonM }
