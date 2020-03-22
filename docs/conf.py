4656f497e3e2c9cd0a720fd577b612efb2bd6acd
Justus Fluegel <klettelars@gmail.com>
2020-03-22 21:56:45 +0100

    add config index file docs

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,8 @@ language = 'de'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+master_doc = "index"
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
