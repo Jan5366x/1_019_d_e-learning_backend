fe5f87e1dbc6f9c6058077372d2bbfdb64b8da00
Schotti <Christopher.Schott@web.de>
2020-03-22 09:32:24 +0100

    change Errorhandling in Controller to ExpressError

--- app/lesson/controllers.ts
+++ app/lesson/controllers.ts
@@ -1,6 +1,7 @@
 import { RequestHandler, Request, Response } from "express";
 import mongoose, { Query } from "mongoose";
 import Lesson from "./model"
+import ExpressError from "../classes/ExpressError";
 const Create: RequestHandler = (req: Request, res: Response) => {
     // Create new Lesson
     // Save in MongoDB
@@ -42,22 +43,22 @@ const ReadByTitle: RequestHandler = (req: Request, res: Response) => {
     });
 };
 
-const Update: RequestHandler = (req: Request, res: Response) => {
+const Update: RequestHandler = (req: Request, res: Response, next: Function) => {
     // Update Lesson
     // Save in MongoDB
     console.log(req.params.id + " ; " + req.body.title)
     var result = Lesson.updateOne({_id: req.params.id}, {title: req.body.title}).exec(function(err:Error){
-        if(err) return err; 
+        return next(new ExpressError("COULD_NOT_UPDATE", ""+err, 400)); 
     });
 
     res.status(200).json({ message: "OK!" });
 };
 
-const Delete: RequestHandler = (req: Request, res: Response) => {
+const Delete: RequestHandler = (req: Request, res: Response, next: Function) => {
     // Delete Lesson
     // Save in MongoDB
     Lesson.deleteOne({_id: req.params.id}, function(err){
-        if(err) return err; 
+        return next(new ExpressError("COULD_NOT_UPDATE", ""+err, 400)); 
     }); 
     res.status(200).json({ message: "OK!" });
 };
