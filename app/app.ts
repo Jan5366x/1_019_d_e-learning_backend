2a5920226d1e808deafe3ac741890b7b9f05280a
Justus Fluegel <klettelars@gmail.com>
2020-03-25 13:26:38 +0100

    Add allow max hader

--- app/app.ts
+++ app/app.ts
@@ -48,6 +48,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.header("Access-Control-Max-Age", "86400")
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
